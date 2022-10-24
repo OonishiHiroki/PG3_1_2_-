a26256c7aa68964de242ce82eec719aa7a1476c1
OonisiHiroki <k021g1328@g.neec.ac.jp>
2022-10-24 09:01:28 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int RecoursiveSalary(int hour) {
+
+	if (hour == 1) {
+		return (100);
+	}
+	return RecoursiveSalary(hour - 1) * 2 - 50;
+
+}
+
+int main() {
+
+	int price = 0;
+	int hour = 1;
+	int result = 0;
+
+
+	for (int i = 1; i < 10; i++) {
+		price += 1072;
+		printf("%dŽžŠÔ‚Å%d‰~\n", i, price);
+
+		result += RecoursiveSalary(i);
+		printf("%dŽžŠÔ‚Å%d‰~\n\n", i, result);
+	}
+	return 0;
+}
