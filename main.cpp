ded8f363ef67b0b83ff0b7bad58ff9ef22fbaed8
OonisiHiroki <k021g1328@g.neec.ac.jp>
2022-11-21 11:18:43 +0900

    STLとコンテナクラス

--- main.cpp
+++ main.cpp
@@ -1,51 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
-#include <time.h>
-#include <functional>
+#include <vector>
+#include <list>
 
-typedef void (*PFunc)(int);
+using namespace std;
 
-void DiceResult(int number) {
-	srand(time(nullptr));
-	int dice = rand() % 6 + 1;
+int main() {
+	//1970年の山手線
+	const char* yamanoteLine[28] = {
 
-	if (dice % 2 == number) {
-		printf("当たり\n");
 	}
-	else {
-		printf("はずれ\n");
-	}
-	printf("サイコロの出目は%dでした\n", dice);
-};
-
-int main(void) {
-
-	PFunc p;
-
-	int number = 0;
-	int second = 3;
-
-	printf("start\n");
-
-	printf("半(奇数)なら[1],丁(偶数)なら[0]を入力してください\n");
-
-	scanf_s("%d", &number);
-
-	std::function<void(PFunc, int)>SetTimeout = [=](PFunc p, int second) {
-		printf("結果...");
-
-		//コールバック関数を呼び出す
-		Sleep(second * 1000);
-
-		//macやUnix系OSの場合
-		//sleep(second);
-		p(number);
-	};
-
-	p = DiceResult;
-
-	SetTimeout(p, second);
-
-	return 0;
 }
