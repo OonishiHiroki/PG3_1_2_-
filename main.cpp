90ad70c99fe830c8e3766fdbfc00a25491b6774d
OonisiHiroki <k021g1328@g.neec.ac.jp>
2022-12-02 17:27:17 +0900

    単方向リスト(char型)(完成)

--- main.cpp
+++ main.cpp
@@ -11,22 +11,50 @@ typedef struct cell {
 
 void create(CELL* endlist, const char* buf);
 void index(CELL* list);
+void deleteCell(CELL* endlist);
 
 int main() {
 	char str[8];
+	int num = 0;
 	//æ“ª‚ÌƒZƒ‹‚ðéŒ¾
 	CELL head;
 	head.next = nullptr;
 
 	while (true) {
-		printf("•¶Žš‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
 
-		printf("“ü—Í‚µ‚½•¶ŽšF");
-		scanf_s("%s", str, 8);
+		if (num == 0) {
+			printf("[—v‘f‚Ì‘€ì]\n");
+			printf("1.—v‘f‚Ìˆê——•\Ž¦\n");
+			printf("2.ÅŒã”ö‚É—v‘f‚Ì‘}“ü\n");
+			printf("3.ÅŒã”ö‚Ì—v‘f‚Ìíœ\n\n");
+			printf("---------------------\n");
+			printf("‘€ì‚ð‘I‘ð‚µ‚Ä‚­‚¾‚³‚¢\n\n");
+		}
 
-		create(&head, str);
+		scanf_s("%d", &num);
 
-		index(&head);
+		if (num == 1) {
+			printf("—v‘f‚Ìˆê——");
+			index(&head);
+			printf("0‚Å‰Šú‰æ–Ê‚É–ß‚é\n");
+		}
+
+		if (num == 2) {
+			printf("“ü—Í‚µ‚½•¶ŽšF");
+			scanf_s("%s", str, 8);
+			printf("\n");
+			create(&head, str);
+			printf("—v‘f[%s]‚ªƒŠƒXƒg‚É‘}“ü‚³‚ê‚Ü‚µ‚½\n\n", str);
+			printf("---------------------\n");
+			printf("0‚Å‰Šú‰æ–Ê‚É–ß‚é\n\n");
+		}
+
+		if (num == 3) {
+			printf("—v‘f‚Ìíœ\n");
+			printf("------------------\n");
+			deleteCell(&head);
+		}
+
 	}
 	return 0;
 }
@@ -39,8 +67,6 @@ void create(CELL* endlist, const char* buf) {
 	strcpy_s(newCell->str, 8, buf);
 	newCell->next = nullptr;
 
-	//NewCell->&str = val;
-
 	while (endlist->next != nullptr) {
 		endlist = endlist->next;
 	}
@@ -48,10 +74,20 @@ void create(CELL* endlist, const char* buf) {
 }
 
 void index(CELL* endlist) {
+	int num = -1;
 	printf("{");
 	while (endlist->next != nullptr) {
+		num++;
 		endlist = endlist->next;
-		printf("%s,", endlist->str);
+		printf("\n%d:%s,",num,endlist->str);
 	}
-	printf("}\n\n");
+	printf("\n}\n\n");
+}
+
+void deleteCell(CELL* endlist) {
+	CELL* list;
+	list = endlist->next;
+	if (endlist->next != nullptr) {
+		list->next = nullptr;
+	}
 }
