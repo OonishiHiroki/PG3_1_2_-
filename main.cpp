f1253384161c85a46e9c80fa3bdeae0824f3de1e
OonisiHiroki <k021g1328@g.neec.ac.jp>
2022-12-02 10:54:06 +0900

    単方向リスト(char型)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <vector>
-#include <list>
+#include <string.h>
+#include <assert.h>
 
-using namespace std;
+//単方向リストの構造体の定義
+typedef struct cell {
+	char str[8];
+	struct cell* next;
+}CELL;
+
+void create(CELL* endlist, const char* buf);
+void index(CELL* list);
 
 int main() {
-	//1970年の山手線
-	const char* yamanoteLine[28] = {
+	char str[8];
+	//先頭のセルを宣言
+	CELL head;
+	head.next = nullptr;
 
+	while (true) {
+		printf("文字を入力してください\n");
+
+		printf("入力した文字：");
+		scanf_s("%s", str, 8);
+
+		create(&head, str);
+
+		index(&head);
 	}
+	return 0;
+}
+
+void create(CELL* endlist, const char* buf) {
+	CELL* newCell;
+	newCell = (CELL*)malloc(sizeof(CELL));
+
+	assert(newCell);
+	strcpy_s(newCell->str, 8, buf);
+	newCell->next = nullptr;
+
+	//NewCell->&str = val;
+
+	while (endlist->next != nullptr) {
+		endlist = endlist->next;
+	}
+	endlist->next = newCell;
+}
+
+void index(CELL* endlist) {
+	printf("{");
+	while (endlist->next != nullptr) {
+		endlist = endlist->next;
+		printf("%s,", endlist->str);
+	}
+	printf("}\n\n");
 }
