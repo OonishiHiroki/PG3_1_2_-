30b9675a2144ca42917ec5510a114c1365d4dd1a
OonisiHiroki <k021g1328@g.neec.ac.jp>
2022-12-02 11:10:11 +0900

    単方向リスト(int型)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <vector>
-#include <list>
+#include<stdlib.h>
+#include<assert.h>
 
-using namespace std;
+typedef struct cell {
+	int val;
+	struct cell* next;
+}CELL;
+
+void create(CELL* list, int val);
+void index(CELL* list);
 
 int main() {
-	//1970年の山手線
-	const char* yamanoteLine[28] = {
+	int val;
+	//先頭のセルを宣言
+	CELL head;
+	head.next = nullptr;
 
+	while (true) {
+		printf("数字を入力してください\n");
+
+		printf("入力した数字：");
+		scanf_s("%d", &val);
+
+		create(&head, val);
+
+		index(&head);
 	}
+	return 0;
+}
+
+void create(CELL* list, int val) {
+	CELL* newCell;
+	newCell = (CELL*)malloc(sizeof(CELL));
+
+	assert(newCell);
+	newCell->val = val;
+	newCell->next = nullptr;
+
+	while (list->next != nullptr) {
+		list = list->next;
+	}
+	list->next = newCell;
+}
+
+void index(CELL* list) {
+	printf("{");
+	while (list->next != nullptr) {
+		list = list->next;
+		printf("%d,", list->val);
+	}
+	printf("}\n\n");
 }
